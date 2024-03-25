7afe221bf8c651690e0747e88fb829f089b68d8b
Zaid Slimane <102682510+ZaidSlimane@users.noreply.github.com>
2024-03-25 15:10:54 +0100

    #landing_page_comp_pxl_btn_

---
+++ Gamified E-learning Platform/src/components/pixelatedButton/pixelatedButton.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import './pixelatedButton.css';
+
+
+function PixelatdButton (){
+
+
+    return (
+        <>
+            <button className="outerclip v2">
+                <div className="innerclip">Start</div>
+            </button>
+
+        </>
+    )
+}
+
+export default PixelatdButton;
